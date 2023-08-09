98fa83247450b9a06619d232a320574ca604d95d
Fire8131113 <106763680+Fire8131113@users.noreply.github.com>
2023-08-09 16:51:43 +0800

    strip head and shoulder rn owow

--- Assets/Scripts/Gay/boomtest2.cs
+++ Assets/Scripts/Gay/boomtest2.cs
@@ -6,39 +6,48 @@ public class boomtest2 : MonoBehaviour
 {
     public GameObject stripper;
 
+    void resetBox(GameObject box)
+    {
+        Destroy(box.GetComponent<BoxCollider>());
+        box.AddComponent<BoxCollider>();
+    }
+
     void strip(GameObject stripper, GameObject wire)
     {
+        GameObject newWire = Instantiate(wire);
+        newWire.name = "New Wire";
+        newWire.transform.SetParent(wire.transform);
+        LineRenderer newWireRen = newWire.GetComponent<LineRenderer>();
         LineRenderer wireRen = wire.GetComponent<LineRenderer>();
         Vector3 point0 = wireRen.GetPosition(0);
         Vector3 point1 = wireRen.GetPosition(1);
-        GameObject newWire = Instantiate(wire);
-        newWire.transform.SetParent(wire.transform);
-        newWire.name = "New Wire";
-        LineRenderer newWireRen = newWire.GetComponent<LineRenderer>();
+        Vector3 pos = stripper.transform.position - wire.transform.position;
 
-        float mid = (point1.z - point0.z) / 2;
-        if(stripper.transform.position.x < mid)
+        float mid = (point1.x - point0.x)/ 2;
+        print(mid);
+        if (pos.x < mid)
         {
-            Debug.Log("left");
-            wireRen.SetPosition(0, new Vector3(point0.x, point0.y, stripper.transform.position.x));
-            newWireRen.SetPosition(1, new Vector3(point0.x, point0.y, stripper.transform.position.x));
+            wireRen.SetPosition(0, new Vector3(pos.x, point0.y, point0.z));
+            newWireRen.SetPosition(1, new Vector3(pos.x, point0.y, point0.z));
             newWireRen.material.color = Color.white;
         }
         else
         {
-            Debug.Log("right");
-            wireRen.SetPosition(1, new Vector3(point1.x, point1.y, stripper.transform.position.x));
-            newWireRen.SetPosition(0, new Vector3(point1.x, point1.y, stripper.transform.position.x));
+            wireRen.SetPosition(1, new Vector3(pos.x, point1.y, point1.z));
+            newWireRen.SetPosition(0, new Vector3(pos.x, point1.y, point1.z));
             newWireRen.material.color = Color.white;
         }
+        resetBox(newWire);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "wire")
         {
-            GameObject wire = collision.gameObject;
-            strip(stripper, wire);
+            if (collision.gameObject.transform.childCount < 2)
+            {
+                strip(stripper, collision.gameObject);
+            }
         }
     }
 }
