2d66bb01d538d31a984159c3953c8148d5d1efc3
Pacarius <pcpgsk@gmail.com>
2023-08-10 22:13:40 +0800

    Finished up wire controller, setting branch on fire.

--- Assets/Scripts/MR/Wires/WireController.cs
+++ Assets/Scripts/MR/Wires/WireController.cs
@@ -10,13 +10,14 @@ public class WireController : MonoBehaviour
     */
     float Scale => MRWireHandler.Scale;
     public bool Init = false;
-    LineRenderer[] StrippedSegs = new LineRenderer[2];
-    public List<Vector3> Vertices = new(){};
+    public bool VertBasedUpdate = false;
+    public LineRenderer[] StrippedSegs = new LineRenderer[2];
+    public List<Vector3> Vertices = new() { };
     public float[] Margins = new float[2]{
         0.5f, 0.5f
     };
-    int boolToInt(bool ballin) => ballin ? 1 : 0;
     public LineRenderer Self;
+    public Material marginMat = null;
     void Awake()
     {
         print("WHAT");
@@ -25,34 +26,23 @@ public class WireController : MonoBehaviour
     }
     void Start()
     {
-        if (Init) TestInit();
+        if (Init)
+        {
+            SpawnMargins test = gameObject.AddComponent<SpawnMargins>();
+            test.controller = this;
+            test.Init();
+        }
         for (int i = 0; i < StrippedSegs.Length; i++)
         {
             Vertices.Add(StrippedSegs[i].GetPosition(0));
         }
     }
+    void FixedUpdate(){
+        if (VertBasedUpdate) UpdateLine();
+    }
     public float GetDistance(int V1, int V2)
     {
         return Vector3.Distance(Vertices[V1], Vertices[V2]);
-    }
-    void TestInit()
-    {
-        bool temp = false;
-        Vector3[] newVecs = new Vector3[2];
-        for (int i = 0; i < StrippedSegs.Length; i++)
-        {
-            LineRenderer tempLine = Instantiate(gameObject, transform).GetComponent<LineRenderer>();
-            Vector3 TargetVec = Vector3.MoveTowards(Self.GetPosition(boolToInt(temp)), gameObject.transform.position, Margins[boolToInt(temp)] * Scale);
-            tempLine.SetPositions(new Vector3[]{
-                Self.GetPosition(boolToInt(temp)),
-                TargetVec
-            });
-            tempLine.material.color = Color.black;
-            newVecs[i] = TargetVec;
-            temp = !temp;
-            StrippedSegs[i] = tempLine;
-        }
-        Self.SetPositions(newVecs);
     }
     void CheckParent()
     {
@@ -65,11 +55,37 @@ public class WireController : MonoBehaviour
     }
     public void UpdateLine()
     {
-        for (int i = 0; i < Vertices.Count; i++)
+        Vector3[] marginVertices = new Vector3[2];
+        //Sets values for first disconnected segment of the wire(Stripped).
+        StrippedSegs[0].SetPosition(0, Vertices[0]);
+        marginVertices[0] = Vector3.MoveTowards(Vertices[0], Vertices[1], Margins[0] * Scale);
+        StrippedSegs[0].SetPosition(1, marginVertices[0]);
+
+
+        StrippedSegs[1].SetPosition(0, Vertices[^1]);
+        marginVertices[1] = Vector3.MoveTowards(Vertices[^1], Vertices[^2], Margins[1] * Scale);
+        StrippedSegs[1].SetPosition(1, marginVertices[1]);
+
+        if (Vertices.Count > 2)
         {
-            if(i == 0){
-                StrippedSegs[0].SetPosition(i, Vertices[0]);
-            }
+            List<Vector3> tempVerts = new(Vertices);
+            tempVerts[0] = marginVertices[0];
+            tempVerts[^1] = marginVertices[1];
+            Self.positionCount = tempVerts.Count;
+            Self.SetPositions(tempVerts.ToArray());
         }
+        else{
+            Self.positionCount = 2;
+            Self.SetPositions(marginVertices);
+            foreach(Vector3 margVec in marginVertices) print(margVec.ToString());
+        }
+    }
+    public void BakeCollider()
+    {
+        MeshCollider collider = GetComponent<MeshCollider>();
+        if (collider == null) collider = gameObject.AddComponent<MeshCollider>();
+        Mesh mesh = new();
+        Self.BakeMesh(mesh, true);
+        collider.sharedMesh = mesh;
     }
 }
